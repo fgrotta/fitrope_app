72aef22a164eb354774a7b0cd1fd44fa45fd5665
dellarosamarco <dellarosamarco00@gmail.com>
2024-12-12 01:02:34 +0100

    feat: handle loader on login

--- lib/authentication/login.dart
+++ lib/authentication/login.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: avoid_print
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fitrope_app/api/getUserData.dart';
+import 'package:fitrope_app/state/actions.dart';
+import 'package:fitrope_app/state/store.dart';
 import 'package:fitrope_app/types/fitropeUser.dart';
 
 class SignInResponse {
@@ -14,6 +16,7 @@ class SignInResponse {
 }
 
 Future<SignInResponse> signInWithEmailPassword(String email, String password) async {
+  store.dispatch(StartLoadingAction());
   try {
     UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
       email: email,
@@ -25,6 +28,8 @@ Future<SignInResponse> signInWithEmailPassword(String email, String password) as
     if(userCredential.user != null) {
       String uid = userCredential.user!.uid;
       Map<String, dynamic>? userData = await getUserData(uid);
+
+      store.dispatch(FinishLoadingAction());
 
       if(userData != null) {
         return SignInResponse(user: FitropeUser.fromJson(userData), error: "");
@@ -48,5 +53,6 @@ Future<SignInResponse> signInWithEmailPassword(String email, String password) as
     print(e);
   }
 
+  store.dispatch(FinishLoadingAction());
   return SignInResponse(error: "Email o password sbagliati");
 }
--- lib/pages/welcome/LoginPage.dart
+++ lib/pages/welcome/LoginPage.dart
@@ -4,11 +4,14 @@ import 'dart:math';
 import 'package:fitrope_app/authentication/isLogged.dart';
 import 'package:fitrope_app/authentication/login.dart';
 import 'package:fitrope_app/components/custom_text_field.dart';
+import 'package:fitrope_app/components/loader.dart';
 import 'package:fitrope_app/router.dart';
 import 'package:fitrope_app/state/actions.dart';
+import 'package:fitrope_app/state/state.dart';
 import 'package:fitrope_app/state/store.dart';
 import 'package:fitrope_app/style.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -53,52 +56,62 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        iconTheme: const IconThemeData(color: Colors.white),
-        title: const Text("Login", style: TextStyle(color: Colors.white)),
-      ),
-      backgroundColor: primaryColor,
-      body: Padding(
-        padding: EdgeInsets.only(left: pagePadding, right: pagePadding, bottom: pagePadding, top: pagePadding + MediaQuery.of(context).viewPadding.top),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return StoreConnector<AppState, bool>(
+      converter: (store) => store.state.isLoading,
+      builder: (context, isLoading) {
+        return Stack(
           children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text('Email', style: TextStyle(color: ghostColor),),
-                const SizedBox(height: 10,),
-                CustomTextField(controller: _emailController, hintText: 'Enter your email',),
-                const SizedBox(height: 20,),
-                const Text('Password', style: TextStyle(color: ghostColor),),
-                const SizedBox(height: 10,),
-                CustomTextField(controller: _passwordController, hintText: 'Enter your password', obscureText: true,),
-                const SizedBox(height: 10,),
-                if(loginError != null) Text(loginError!, style: const TextStyle(color: dangerColor),) 
-              ],
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width - pagePadding * 2,
-              child: ElevatedButton(
-                onPressed: () {
-                  onLogin();
-                },
-                style: ButtonStyle(
-                  backgroundColor: WidgetStateProperty.all(secondaryColor),
-                  shape: WidgetStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    )
-                  )
-                ), 
-                child: const Text('Login', style: TextStyle(color: Colors.white),),
+            Scaffold(
+              appBar: AppBar(
+                backgroundColor: Colors.transparent,
+                iconTheme: const IconThemeData(color: Colors.white),
+                title: const Text("Login", style: TextStyle(color: Colors.white)),
               ),
-            )
+              backgroundColor: primaryColor,
+              body: Padding(
+                padding: EdgeInsets.only(left: pagePadding, right: pagePadding, bottom: pagePadding, top: pagePadding + MediaQuery.of(context).viewPadding.top),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text('Email', style: TextStyle(color: ghostColor),),
+                        const SizedBox(height: 10,),
+                        CustomTextField(controller: _emailController, hintText: 'Enter your email',),
+                        const SizedBox(height: 20,),
+                        const Text('Password', style: TextStyle(color: ghostColor),),
+                        const SizedBox(height: 10,),
+                        CustomTextField(controller: _passwordController, hintText: 'Enter your password', obscureText: true,),
+                        const SizedBox(height: 10,),
+                        if(loginError != null) Text(loginError!, style: const TextStyle(color: dangerColor),) 
+                      ],
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width - pagePadding * 2,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          onLogin();
+                        },
+                        style: ButtonStyle(
+                          backgroundColor: WidgetStateProperty.all(secondaryColor),
+                          shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            )
+                          )
+                        ), 
+                        child: const Text('Login', style: TextStyle(color: Colors.white),),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ),
+            if (isLoading) const Loader(),
           ],
-        ),
-      )
+        );
+      }
     );
   }
 }
