c2664b085706ab632a6021cb48505f26543b1a40
Francesco Grotta <grotta.francesco@gmail.com>
2025-07-30 10:05:36 +0200

    Change Workflow

--- lib/api/courses/deleteCourse.dart
+++ lib/api/courses/deleteCourse.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fitrope_app/api/courses/unsubscribeToCourse.dart';
 import 'package:fitrope_app/api/courses/getCourses.dart';
